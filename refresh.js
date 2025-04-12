644396ede07efe215b555fb1b1073fbb8b3ed9b6
felipeCisotti <felipe.cisotti@aluno.senai.br>
2025-04-12 17:21:45 -0300

    ALTERADO

--- refresh.js
+++ refresh.js
@@ -3,3 +3,8 @@ const h1 = document.querySelector("#refresh")
 h1.addEventListener("click", () => {
     location.reload()
 })
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-links');
+    nav.classList.toggle('open');
+}
